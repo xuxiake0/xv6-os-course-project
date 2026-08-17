68351b75cae4daaf373c436aac0dc08e82c68aa3
xuxiake0 <zhu32609@gmail.com>
2026-08-18 01:30:08 +0800

    pgtbl: print the init process page table

--- kernel/vm.c
+++ kernel/vm.c
@@ -65,6 +65,30 @@ kvminithart()
   sfence_vma();
 }
 
+static void
+vmprintwalk(pagetable_t pagetable, int depth)
+{
+  for(int i = 0; i < 512; i++){
+    pte_t pte = pagetable[i];
+    if((pte & PTE_V) == 0)
+      continue;
+
+    for(int level = 0; level < depth; level++)
+      printf(" ..");
+    printf("%d: pte %p pa %p\n", i, pte, PTE2PA(pte));
+
+    if(PTE_FLAGS(pte) == PTE_V)
+      vmprintwalk((pagetable_t)PTE2PA(pte), depth + 1);
+  }
+}
+
+void
+vmprint(pagetable_t pagetable)
+{
+  printf("page table %p\n", pagetable);
+  vmprintwalk(pagetable, 1);
+}
+
 // Return the address of the PTE in page table pagetable
 // that corresponds to virtual address va.  If alloc!=0,
 // create any required page-table pages.
