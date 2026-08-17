4fd28b33a63437a3c85a8186da591d3fab2dd380
xuxiake0 <zhu32609@gmail.com>
2026-08-18 01:13:06 +0800

    lab(util): implement find

---
+++ user/find.c
@@ -0,0 +1,81 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "kernel/fs.h"
+#include "kernel/fcntl.h"
+#include "user/user.h"
+
+static char*
+basename(char *path)
+{
+  char *p;
+
+  p = path + strlen(path);
+  while(p > path && p[-1] != '/')
+    p--;
+  return p;
+}
+
+static void
+find(char *path, char *target)
+{
+  char buf[512];
+  char *p;
+  int fd;
+  struct dirent de;
+  struct stat st;
+
+  if((fd = open(path, O_RDONLY)) < 0){
+    fprintf(2, "find: cannot open %s\n", path);
+    return;
+  }
+
+  if(fstat(fd, &st) < 0){
+    fprintf(2, "find: cannot stat %s\n", path);
+    close(fd);
+    return;
+  }
+
+  if(strcmp(basename(path), target) == 0)
+    printf("%s\n", path);
+
+  if(st.type != T_DIR){
+    close(fd);
+    return;
+  }
+
+  if(strlen(path) + 1 + DIRSIZ + 1 > sizeof(buf)){
+    fprintf(2, "find: path too long: %s\n", path);
+    close(fd);
+    return;
+  }
+
+  strcpy(buf, path);
+  p = buf + strlen(buf);
+  *p++ = '/';
+
+  while(read(fd, &de, sizeof(de)) == sizeof(de)){
+    if(de.inum == 0)
+      continue;
+
+    memmove(p, de.name, DIRSIZ);
+    p[DIRSIZ] = 0;
+    if(strcmp(p, ".") == 0 || strcmp(p, "..") == 0)
+      continue;
+
+    find(buf, target);
+  }
+
+  close(fd);
+}
+
+int
+main(int argc, char *argv[])
+{
+  if(argc != 3){
+    fprintf(2, "usage: find path filename\n");
+    exit(1);
+  }
+
+  find(argv[1], argv[2]);
+  exit(0);
+}
